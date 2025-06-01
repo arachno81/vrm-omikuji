1fd23ff009eb32d23c9d0c6462756626525c9021
arachno81 <hizmism225ch@outlook.jp>
2025-06-01 17:48:23 +0900

    feat: add OmikujiPage view component

---
+++ src/views/OmikujiPage.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h1>VRMおみくじ</h1>
+	</div>
+
+	<!-- アップロード -->
+	<div>
+		<p></p>
+		<input type="file" />
+	</div>
+
+	<!-- プレビュー -->
+	<div>
+		<canvas />
+	</div>
+
+	<!-- ボタン -->
+	<div>
+		<button></button>
+		<button></button>
+	</div>
+
+	<!-- SNSシェア -->
+	<div>
+		<button></button>
+		<button></button>
+	</div>
+</template>
