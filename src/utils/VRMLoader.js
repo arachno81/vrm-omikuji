92a1c91a0e4084ecd41579863b70bcf3412c574b
arachno81 <hizmism225ch@outlook.jp>
2025-06-02 12:45:04 +0900

    fix: correct GLTFLoader import path to fix runtime error in VRMLoader

--- src/utils/VRMLoader.js
+++ src/utils/VRMLoader.js
@@ -1,15 +1,16 @@
 import * as THREE from 'three';
 import { VRMLoaderPlugin, VRMUtils, VRM } from '@pixiv/three-vrm';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-const loader = new THREE.GLTFLoader();
+const loader = new GLTFLoader();
 loader.register((parser) => new VRMLoaderPlugin(parser));
 
 export async function loadVRMFromFile(file){
 	const arrayBuffer = await file.arrayBuffer();
+
 	return await new Promise((resolve, reject)=>{
 		loader.parse(arrayBuffer, '',(gltf)=>{
 			const vrm = gltf.userData.vrm;
-			VRMUtils.removeUnnecessaryJoints(vrm.scene);
 			resolve(vrm);
 		}, reject);
 	});
